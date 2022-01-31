07bbe29673c3b37618936d04d229a12648efdda9
kbld <54550659+kbld@users.noreply.github.com>
2022-01-31 09:24:13 +0100

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/* $(document).ready(function () {
     $(window).scroll(function () {
       var scroll = $(window).scrollTop();
       if (scroll > 90) {
@@ -14,7 +14,88 @@ $(document).ready(function () {
         var styleElem = document.head.appendChild(
           document.createElement("style")
         );
-        styleElem.innerHTML = ".navbar {background :linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.7497313753581663) 31%, rgba(0,0,0,1) 98%);;}";
+        styleElem.innerHTML = ".navbar {background :  background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%);}";
       }
     });
-  });
+  }); */
+
+  (function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    //I'm adding this section so I don't have to keep updating this pen every year :-)
+    //remove this if you don't need it
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "04/30/",
+        birthday = dayMonth + yyyy;
+    
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > birthday) {
+      birthday = dayMonth + nextYear;
+    }
+    //end
+    
+    const countDown = new Date(birthday).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          //do something later when date is reached
+          if (distance < 0) {
+            document.getElementById("headline").innerText = "It's my birthday!";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+          }
+          //seconds
+        }, 0)
+    }());
+
+
+
+  var isAlreadyRun = false;
+
+$(window).scroll( function(){
+
+    $('.counter-show').each( function(i){
+
+        var bottom_of_object = $(this).position().top + $(this).outerHeight() / 2;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+
+            if( bottom_of_window > ( bottom_of_object + 20 )  ){
+				if (!isAlreadyRun) {
+					$('.count-number').each(function () {
+	            	
+	                $(this).prop('Counter', 0).animate({
+	                    Counter: $(this).text()
+	                }, {
+	                        duration: 3500,
+	                        easing: 'swing',
+	                        step: function (now) {
+	                            $(this).text(Math.ceil(now));
+	                        }
+	                    });
+	            	});
+				}
+                isAlreadyRun = true;
+            }
+    }); 
+
+});
+
+
+
+
