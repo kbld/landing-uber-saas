83ce4787dc26fd5c36291bf075f9953d0aa6b4c2
kbld <54550659+kbld@users.noreply.github.com>
2022-02-02 22:10:30 +0100

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,73 @@
-/* $(document).ready(function () {
-    $(window).scroll(function () {
-      var scroll = $(window).scrollTop();
-      if (scroll > 90) {
-        $(".navbar").css("position", "fixed");
-        $(".navbar").css("padding", "37px 50px");
-        var styleElem = document.head.appendChild(
-          document.createElement("style")
-        );
-        styleElem.innerHTML = ".navbar {background: #000;}";
-      } else {
-        $(".navbar").css("position", "absolute");
-        $(".navbar").css("padding", "57px 50px");
-        var styleElem = document.head.appendChild(
-          document.createElement("style")
-        );
-        styleElem.innerHTML = ".navbar {background :  background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%);}";
-      }
-    });
-  }); */
+/* let seconds = seconds,
+  minutes = seconds * 60,
+  hours = minutes * 60,
+  days = hours * 24;
 
-  (function () {
-    const second = 1000,
-          minute = second * 60,
-          hour = minute * 60,
-          day = hour * 24;
-  
-    //I'm adding this section so I don't have to keep updating this pen every year :-)
-    //remove this if you don't need it
-    let today = new Date(),
-        dd = String(today.getDate()).padStart(2, "0"),
-        mm = String(today.getMonth() + 1).padStart(2, "0"),
-        yyyy = today.getFullYear(),
-        nextYear = yyyy + 1,
-        dayMonth = "04/30/",
-        birthday = dayMonth + yyyy;
-    
-    today = mm + "/" + dd + "/" + yyyy;
-    if (today > birthday) {
-      birthday = dayMonth + nextYear;
-    }
-    //end
-    
-    const countDown = new Date(birthday).getTime(),
-        x = setInterval(function() {    
-  
-          const now = new Date().getTime(),
-                distance = countDown - now;
-  
-          document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-  
-          //do something later when date is reached
-          if (distance < 0) {
-            document.getElementById("headline").innerText = "It's my birthday!";
-            document.getElementById("countdown").style.display = "none";
-            document.getElementById("content").style.display = "block";
-            clearInterval(x);
+var date_actuelle = Date.now();
+var date_final = new Date("Mar 1 00:00:00 2022");
+
+getDate(), getMonth(), getFullYear();
+
+let h = math.floor(gap % jour(heure));
+
+document.getElementById("demo").innerHTML = ms;
+
+
+*/
+
+
+function reveal() {
+        var reveals = document.querySelectorAll(".reveal, .reveal-title, .reveal-p ");
+      
+        for (var i = 0; i < reveals.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = reveals[i].getBoundingClientRect().top;
+          var elementVisible = 150;
+      
+          if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+          } else {
+            reveals[i].classList.remove("active");
           }
-          //seconds
-        }, 0)
-    }());
+        }
+      }
+      
+      window.addEventListener("scroll", reveal);
 
+/* $(function () {
+  var Accordion = function (el, multiple) {
+    this.el = el || {};
+    this.multiple = multiple || false;
 
+    var links = this.el.find(".article-title");
+    links.on(
+      "click",
+      {
+        el: this.el,
+        multiple: this.multiple,
+      },
+      this.dropdown
+    );
+  };
 
-  var isAlreadyRun = false;
+  Accordion.prototype.dropdown = function (e) {
+    var $el = e.data.el;
+    ($this = $(this)), ($next = $this.next());
 
-$(window).scroll( function(){
+    $next.slideToggle();
+    $this.parent().toggleClass("open");
 
-    $('.counter-show').each( function(i){
-
-        var bottom_of_object = $(this).position().top + $(this).outerHeight() / 2;
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-
-            if( bottom_of_window > ( bottom_of_object + 20 )  ){
-				if (!isAlreadyRun) {
-					$('.count-number').each(function () {
-	            	
-	                $(this).prop('Counter', 0).animate({
-	                    Counter: $(this).text()
-	                }, {
-	                        duration: 3500,
-	                        easing: 'swing',
-	                        step: function (now) {
-	                            $(this).text(Math.ceil(now));
-	                        }
-	                    });
-	            	});
-				}
-                isAlreadyRun = true;
-            }
-    }); 
-
+    if (!e.data.multiple) {
+      $el
+        .find(".accordion-content")
+        .not($next)
+        .slideUp()
+        .parent()
+        .removeClass("open");
+    }
+  };
+  var accordion = new Accordion($(".accordion-container"), false);
 });
 
 
-
-
+*/
